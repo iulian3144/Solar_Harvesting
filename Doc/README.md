5442ba003ba4a380f82a9f983832269c2dfb4e9c
GeorgeCh <georgechirica92@gmail.com>
2019-03-13 19:36:25 +0200

    - empty new project

---
+++ Doc/README.md
@@ -0,0 +1,5 @@
+# Solar_Harvesting
+Proiect Licenta Chirica Silviu
+
+
+- PIC18F46K22
---
+++ Solar_Harvesting.X/main.c
@@ -0,0 +1,13 @@
+/*
+ * File:   main.c
+ * Author: georg
+ *
+ * Created on March 13, 2019, 7:35 PM
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    return;
+}
